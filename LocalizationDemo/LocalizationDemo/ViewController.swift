727ec59c93c2fcbd2cc8d228266bb8e122fe286d
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-12 22:17:02 +0300

    Add Turkish and Spanish language

---
+++ LocalizationDemo/LocalizationDemo/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  LocalizationDemo
+//
+//  Created by İbrahim Demirci on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    private let myLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello".localized()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.addSubview(myLabel)
+        myLabel.frame = view.bounds
+    }
+
+
+}
+
+
+extension String {
+    func localized() -> String {
+        return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: self)
+    }
+}
+
